659c990782c5e01075a7bf3cb1e1a28b89226f72
Michael Wenhold <michael@Michaels-MacBook-Pro.local>
2018-12-18 20:07:19 -0600

    add some teams

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import TeamCard from "./components/TeamCard";
+import Title from "./components/Title";
+import Wrapper from "./components/Wrapper";
+import teams from "./teams.json";
 
 class App extends Component {
+// Setting this.state.teams to the teams json array
+state = {
+  teams
+};
+
+removeTeam = id => {
+  // Filter this.state.teams for teams with an id not equal to the id being removed
+  const teams = this.state.teams.filter(team => team.id !== id);
+  // Set this.state.friends equal to the new friends array
+  this.setState({ teams });
+};
+
+// Map over this.state.teams and render a TeamCard component for each team object
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Wrapper>
+        <Title>Football Teams</Title>
+        {this.state.teams.map(team => (
+          <TeamCard
+          removeTeam={this.removeTeam}
+          id={team.id}
+          key={team.id}
+          name={team.name}
+          />
+        ))}
+      </Wrapper>
     );
   }
 }
